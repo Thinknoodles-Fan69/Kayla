502dd000f24a1e01745feb31cc34d68dd6231fe2
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-07-16 08:55:22 +0000

    Jul 16, 2025, 8:55 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -15,11 +15,11 @@ function setup() {
   ball.diameter = 100;
   ball.color = "brown";
 
-  ball = new Sprite();
-  ball.x = 100;
-  ball.y = 300;
-  ball.diameter = 100;
-  ball.color = "brown";
+  ball2 = new Sprite();
+  ball2.x = 200;
+  ball2.y = 300;
+  ball2.diameter = 100;
+  ball2.color = "brown";
 
 
 }
