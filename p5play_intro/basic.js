a8413bcbae324131eb0a9fe6efe865f46d0cd1e8
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-07-23 08:46:05 +0000

    Jul 23, 2025, 8:46 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -18,11 +18,11 @@ function setup() {
 
   let a = 5;
   let b = 10;
-  let sum = a + b
-  let product = a * b
+  let sum = a + b;
+  let product = a * b;
   
-  console.log(sum)
-  console.log(sum)
+  console.log(sum);
+  console.log(product);
 
 }
 
