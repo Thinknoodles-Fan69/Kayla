c8105109366647c7d454f7c1c41d2c81de4c35f8
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-07-23 08:51:27 +0000

    Jul 23, 2025, 8:51 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -27,7 +27,7 @@ function setup() {
   fill(0, 255, 255)
   textSize(14)
   text("The sum of a and b is " + sum, 50, 50)
-  text("The sum of a and b is " + sum, 50, 50)
+  text("The product of a and b is " + product, 50, 50)
 
 }
 
