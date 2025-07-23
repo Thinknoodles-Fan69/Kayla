7325f1675c7efa6a492b97ef6ed18d9236c8d625
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-07-23 07:57:40 +0000

    Jul 23, 2025, 7:57 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -47,11 +47,11 @@ function draw() {
   text("Mouse: (" + int(mouseX) + ", " + int(mouseY) + ")", 10, 40);
 
   if(ball.x < 0 + ball.diameter / 2  || ball.x > width - ball.diameter / 2) {
-    ball.vel.x *= -1
+    ball.vel.x *= -1;
   }
 
   if(ball.y < 0 + ball.diameter / 2  || ball.y > height - ball.diameter / 2) {
-    ball.vel.y *= -1
+    ball.vel.y *= -1;
   }
     
 }
