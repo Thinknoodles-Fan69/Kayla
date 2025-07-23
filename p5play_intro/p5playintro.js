442868122cf74c90b4df57cb6191a0644cd12b5d
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-07-23 07:44:48 +0000

    Jul 23, 2025, 7:44 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -44,5 +44,5 @@ function draw() {
   fill (0);
   textSize(16);
   text("Ball: (" + int(ball.x) + ", " + int(ball.y) + ")", 10, 20);
-  text("Mouse: (" + mouseX + ", " + mouseY + )
+  text("Mouse: (" + mouseX + ", " + mouseY + ")", 10, 20)
 }
