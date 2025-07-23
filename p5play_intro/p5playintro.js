326b296a115709fca1927754208442aa4fdf95c1
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-07-23 08:20:28 +0000

    Jul 23, 2025, 8:20 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -18,7 +18,7 @@ function setup() {
   ball.color = "brown";
   ball.vel.x = 3;
   ball.vel.y = 3;
-  ball.bounciness = ;
+  ball.bounciness = 10000000000000000000000000000000000000000000000;
   ball.collider = "dynamic";
 
 
