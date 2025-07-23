efeb0324c7621bbb73044d2b13cb598023639f9a
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-07-23 08:20:00 +0000

    Jul 23, 2025, 8:20 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -62,8 +62,8 @@ function draw() {
     ball.vel.y *= -1;
   }
 
-  box.x = mouseX;
-  box.y = mouseY;
+  // box.x = mouseX;
+  // box.y = mouseY;
     
   if (mouse.presses("left")) {
     let x = new Sprite (mouseX, mouseY, 30, 30);
