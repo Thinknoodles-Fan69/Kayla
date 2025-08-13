eedb09476ad82e872827dd48e0c49b17cc6c0653
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-08-13 08:18:06 +0000

    Aug 13, 2025, 8:18 AM

--- flappybird/flappybirdcopy.js
+++ flappybird/flappybirdcopy.js
@@ -3,7 +3,7 @@ let flapMidImg, bg, base;
 
 function preload() {
     flapMidImg = loadImage("flappybird/assets/bluebird-midflap.png");
-    bg = loadImage("")
+    bg = loadImage("flappybird/assets/background-day.png")
 }
 
 
