f63af5fb902f060a354e2fd4d3e997f3f4727f89
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-09-03 08:20:44 +0000

    Sep 3, 2025, 8:20 AM

--- flappybird/flappybirdcopy3.js
+++ flappybird/flappybirdcopy3.js
@@ -91,6 +91,7 @@ function draw() {
 
 
         topPipe = new Sprite(400, midY + gap/2 - 200, 52, 320, 'static');
+        topPipe.rotation 
         topPipe.img = pipe;
         pipeGroup.add(topPipe);
         pipeGroup.layer = 0;
