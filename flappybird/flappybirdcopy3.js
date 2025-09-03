c70d2e02d8088bcc855eaa9c73f53b1a45893c46
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-09-03 08:15:20 +0000

    Sep 3, 2025, 8:15 AM

--- flappybird/flappybirdcopy3.js
+++ flappybird/flappybirdcopy3.js
@@ -10,7 +10,7 @@ function preload() {
     flapDownImg = loadImage("assets/bluebird-downflap.png");
     bg = loadImage("assets/background-day.png");
     base = loadImage("assets/base.png");
-    pipe = loadImage("flappybird/assets/pipe-green.png")
+    pipe = loadImage("assets/pipe-green.png")
 }
 
 
