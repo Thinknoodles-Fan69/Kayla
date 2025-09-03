63ac7d9e7665d32c4e4183ce770fdbf0dfffa873
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-09-03 09:01:00 +0000

    Sep 3, 2025, 9:01 AM

--- flappybird/flappybirdcopy4.js
+++ flappybird/flappybirdcopy4.js
@@ -117,13 +117,14 @@ function spawnPipePair() {
 
 
         bottomPipe = new Sprite(400, midY + gap/2 + 200, 52, 320, 'static');
+        bottomPipe = new Sprite(bird.x + 400, midY + gap / 2 + 200, 52, 320, 'static');
         bottomPipe.img = pipe;
         
 
 
         topPipe = new Sprite(400, midY + gap/2 - 300, 52, 320, 'static');
         topPipe = new Sprite(bird.x + 400, midY - gap / 2 - 200, 52, 320, 'static');
-        topPipe.rotation = -180
+        topPipe.rotation = -180;
         topPipe.img = pipe;
 
         pipeGroup.add(bottomPipe);
