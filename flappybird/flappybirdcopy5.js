a133678535f55b2cf90ffda8d2ebd93e27c1f97f
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-09-10 09:00:28 +0000

    Sep 10, 2025, 9:00 AM

--- flappybird/flappybirdcopy5.js
+++ flappybird/flappybirdcopy5.js
@@ -65,7 +65,7 @@ function setup() {
 function draw() {
     image(bg, 0, 0, width, height);
 
-    if(kb.presses('space') || mouse.presses())
+    if(kb.presses('space') || mouse.presses());
 
 
     if(kb.presses("space") || mouse.presses()) {
