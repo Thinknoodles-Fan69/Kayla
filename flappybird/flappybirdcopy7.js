9bcaa100ce392c34c68f198879c2c8f26eca5569
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-09-24 09:00:00 +0000

    Sep 24, 2025, 9:00 AM

--- flappybird/flappybirdcopy7.js
+++ flappybird/flappybirdcopy7.js
@@ -148,7 +148,7 @@ function draw() {
                 startGame = false;
 
 
-                pipe.removeAll();
+                pipeGroup.removeAll();
                 bird.vel.x = 0;
                 bird.vel.y = 0;
                 bird.rotation = 0;
