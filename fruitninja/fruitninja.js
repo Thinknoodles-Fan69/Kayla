0f65d4b1e64152079993dcc66d6acf1afa6df4fe
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-10-01 08:22:31 +0000

    Oct 1, 2025, 8:22 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -49,8 +49,8 @@ function spawnFruit() {
     fruit.image = fruitData.whole;
     fruit.type = fruitData;
     fruit.vel.y = random(-10, -14);
-    fruit.vel.x = random(2, 2);
-
+    fruit.vel.x = random(-2, 2);
+    fruit.friction = 0;
 
 }
 
