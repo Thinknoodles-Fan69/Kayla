d777a3507199f75f91cfa5025b633d38993bfe87
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-10-01 08:31:40 +0000

    Oct 1, 2025, 8:31 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -50,7 +50,7 @@ function spawnFruit() {
 
 
     let fruit = new fruitGroup.Sprite(randomX, height + 20, 40);
-    fruit.image = fruitData.whole;
+    fruit.img = fruitData.whole;
     fruit.type = fruitData;
     fruit.vel.y = random(-10, -14);
     fruit.vel.x = random(-2, 2);
