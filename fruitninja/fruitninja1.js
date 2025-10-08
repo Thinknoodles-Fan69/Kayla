c6536a934761634b5cc733ccd3a19556e5360aa0
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-10-08 08:26:46 +0000

    Oct 8, 2025, 8:26 AM

--- fruitninja/fruitninja1.js
+++ fruitninja/fruitninja1.js
@@ -108,6 +108,7 @@ function splitFruit(x, y, fruitData) {
     left.img = fruitData.half;
     left.vel.x = -3;
     left.vel.y = random(-5, -2);
+    
 
     let right = new fruitsThatAreCutIntoHalfBecauseWeUsedTheMouseToCutTheFruitsBecauseTheGameIsCalledFruitNinja.Sprite(x + 20, y, 40, 40);
     right.img = fruitData.half;
