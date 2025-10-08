8f9be20654b55fe993b3218cf35db945c2ae6009
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-10-08 08:00:56 +0000

    Oct 8, 2025, 8:00 AM

--- fruitninja/fruitninja1.js
+++ fruitninja/fruitninja1.js
@@ -86,7 +86,8 @@ function splitFruit() {
         if (d < ((fruit.d / 2) + 5)) {
             fruit.sliced = true;
 
-            let fx = 
+            let fx = fruit.x;
+            let fy = fruit.y;
 
 
             fruit.remove();
