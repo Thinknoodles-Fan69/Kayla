82c9bc7dd0d209f61e03ab5dd5bc4a9f5784fe73
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-10-15 08:36:30 +0000

    Oct 15, 2025, 8:36 AM

--- fruitninja/fruitninja1.js
+++ fruitninja/fruitninja1.js
@@ -44,7 +44,7 @@ function draw() {
     image(dojoBG, 0, 0, width, height);
 
     if(frameCount % 120 === 0) {
-        let num = 100;
+        let num = 390;
         for(let i = 0; i < num; i++) {
             spawnFruit();
         }
