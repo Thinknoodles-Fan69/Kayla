89d28f177bc2ba8bbe24b01cb22896359c413381
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-10-29 07:53:31 +0000

    Oct 29, 2025, 7:53 AM

--- fruitninja/fruitninja1.js
+++ fruitninja/fruitninja1.js
@@ -31,6 +31,7 @@ function preload() {
 function setup() {
     new Canvas(800, 600);
     world.gravity.y = 10;
+    
     fruitGroup = new Group(); 
     fruitsThatAreCutIntoHalfBecauseWeUsedTheMouseToCutTheFruitsBecauseTheGameIsCalledFruitNinja = new Group();
 }
--- fruitninja/fruitninja2.js
+++ fruitninja/fruitninja2.js
@@ -34,6 +34,7 @@ function preload() {
 function setup() {
     new Canvas(800, 600);
     world.gravity.y = 10;
+    
     fruitGroup = new Group(); 
     fruitsThatAreCutIntoHalfBecauseWeUsedTheMouseToCutTheFruitsBecauseTheGameIsCalledFruitNinja = new Group();
 }
