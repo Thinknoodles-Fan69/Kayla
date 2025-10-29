a35afb9ffe6b453b36972c213e30b83a341d0de0
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-10-29 08:55:16 +0000

    Oct 29, 2025, 8:55 AM

--- fruitninja/fruitninja2.js
+++ fruitninja/fruitninja2.js
@@ -54,7 +54,7 @@ function draw() {
     clear();
     image(dojoBG, 0, 0, width, height);
 
-    if ((kb.presses(" ") || mouse.presses()) && (gameState == "start")) {
+    if ((kb.presses(" ") || mouse.presses()) && (gameState == "start") || gameState === "gameover") {
         gameState = "play";
         score = 0;
         missedFruits = 0;
