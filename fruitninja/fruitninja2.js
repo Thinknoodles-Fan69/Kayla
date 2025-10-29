1f4bb22f16a7158d2344eb16db71a8983e40f4fd
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-10-29 07:55:52 +0000

    Oct 29, 2025, 7:55 AM

--- fruitninja/fruitninja2.js
+++ fruitninja/fruitninja2.js
@@ -109,7 +109,7 @@ function draw() {
 
     gameTimer = floor((millis() - gameStartTime) / 1000);
 
-    text("Time: " + (gameDuration - gameTimer), )
+    text("Time: " + (gameDuration - gameTimer), 400, 10);
 }
 
 
