47175294bc56dcfb48cdfd3dbfba8ea745bcbb04
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-10-29 09:06:12 +0000

    Oct 29, 2025, 9:06 AM

--- fruitninja/fruitninja2.js
+++ fruitninja/fruitninja2.js
@@ -113,11 +113,7 @@ function draw() {
             spawnFruit();
         }
 
-        if(gameTimer >= 55) {
-        for(let i = 0; i < 50; i++) {
-            spawnFruit();
-        }
-    }
+        
 
     }
 
