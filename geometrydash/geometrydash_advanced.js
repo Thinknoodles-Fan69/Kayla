3307b63a8c004ed4436b930c4b170d6534f55be2
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-11-15 09:17:38 +0000

    Nov 15, 2025, 9:17 AM

--- geometrydash/geometrydash_advanced.js
+++ geometrydash/geometrydash_advanced.js
@@ -59,7 +59,7 @@ function draw() {
 
         }
 
-        triangle(spikesX[i], height, spikesX[i] + spikeWidth / 2, spike- spikeHeight, height);
+        triangle(spikesX[i], height, spikesX[i] + spikeWidth / 2, spikesX[i]- spikeHeight, height);
     }
 
     
